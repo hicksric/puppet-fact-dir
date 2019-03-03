d64250dfab878c2c0a20bf2e8773f87b3a776a75
root <root@richardhicks1c.mylabserver.com>
2019-03-04 09:57:39 +1300

    factdir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class factdir {
-  file { '/etc/puppetlab/facter/facts.d':
+  file { ['/etc/puppetlab/facter', '/etc/puppetlab/facter/facts.d']:
     ensure => directory,
-    recurse => true,
   }
 }
